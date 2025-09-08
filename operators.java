bda92d917e8fa2f90bc4d954029a03cf2dfbb451
Ajay7006-mca <ajay7006.mca25@chitkara.edu.in>
2025-09-08 10:17:22 +0530

    Operators

---
+++ operators.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class operators {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the value for first variable:");
+        int a= sc.nextInt();
+        System.out.println("Enter the value for second variable:");
+        int b= sc.nextInt(); 
+        //Arthimetic Operators
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+    sc.close();
+    
+    }
+}
+
